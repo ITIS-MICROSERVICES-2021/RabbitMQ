2640d36eb39ce00da2419639f47f2cd34374ad1d
hernantos <rehsatart@gmail.com>
2021-10-27 00:49:22 +0300

    int2 блин слн пересоздавать придется

---
+++ MSRabbitMQ/DIExtensions/AddRabbitMQSingletonConnectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MSRabbitMQ.Settings;
+using RabbitMQ.Client;
+
+namespace MSRabbitMQ.DIExtensions
+{
+    public static class AddRabbitMQSingletonConnectionExtension
+    {
+        public static IServiceCollection AddRabbitMQSingletonConnection(this IServiceCollection services,
+            RabbitConnectionParameters parameters)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                UserName = parameters.UserName,
+                Password = parameters.Password,
+                HostName = parameters.Host,
+                Port = parameters.Port
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            services.AddSingleton(connection);
+
+            return services;
+        }
+    }
+}
---
+++ MSRabbitMQ/Settings/RabbitConnectionParameters.cs
@@ -0,0 +1,18 @@
+﻿namespace MSRabbitMQ.Settings
+{
+    public class RabbitConnectionParameters
+    {
+        public string UserName { get; }
+        public string Password { get; }
+        public int Port { get; }
+        public string Host { get; }
+        
+        public RabbitConnectionParameters(string userName, string password, int port, string host)
+        {
+            UserName = userName;
+            Password = password;
+            Port = port;
+            Host = host;
+        }
+    }
+}
